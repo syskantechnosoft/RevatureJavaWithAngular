6ffbd105effbfaddb45ee5fe7a3e5b1958f3c226
Siva OS <syskantechnosoft@users.noreply.github.com>
2022-03-16 10:10:59 -0400

    Add files via upload

---
+++ Day27 15Mar2022/docker-java-app/Dockerfile
@@ -0,0 +1,5 @@
+FROM java:8  
+COPY . /var/www/java  
+WORKDIR /var/www/java  
+RUN javac HelloWorld.java  
+CMD ["java", "HelloWorld"]
---
+++ Day27 15Mar2022/docker-java-app/HelloWorld.java
@@ -0,0 +1,5 @@
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("This is a demo Java program for containerization");
+	}
+}
